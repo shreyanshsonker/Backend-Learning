7e38aec2f0ffeac47e07ae95e781661deb021540
Shreyansh Sonker <shreyanshsonkar95@gmail.com>
2026-02-09 16:35:42 +0530

    get post

--- Day_3/src/app.js
+++ Day_3/src/app.js
@@ -4,4 +4,25 @@ const express= require("express")
 
 const app = express() // Storing the instance of a server in a variable app
 
+
+const notes = []
+
+app.use(express.json())
+
+app.post('/notes', (req,res)=> {
+    notes.push (req.body)
+
+    res.status(201).json({
+        message:"note created succesfully" 
+     })
+})
+
+
+app.get("/notes", (req,res)=>{
+     res.status(200).json({
+        notes :notes,
+        message: "notes fetched succesfully"
+     })
+})
+
  module.exports = app
